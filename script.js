4bcb4505411c11c73651cf122c3592880cb9b9eb
DinaLysenko <dinafate@rambler.ru>
2024-03-28 11:57:14 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// data
+const playList = {
+    title: 'Playlist',
+    imageCoverUrl: ['images/coverImage/russian-rock/russian-rock.jpg', 'images/coverImage/rock/rock.jpg'],
+    playListCategory: ['Rock', 'Russian Rock'],
+    tracksCount: 4,
+    tracksDuration: "11m 41s",
+    tracksRussianRock: [
+        {
+            id: 0,
+            trackName: 'tracks/russian-rock/Korol_i_SHut_-_Kukla_kolduna.mp3',
+            isHot: true,
+        },
+        {
+            id: 1,
+            trackName: 'tracks/russian-rock/Korol_i_SHut_-_Lesnik.mp3',
+            isHot: true,
+        },
+        {
+            id: 2,
+            trackName: 'tracks/russian-rock/Korol_i_SHut_-_Prygnu_so_skaly.mp3',
+            isHot: true,
+        },
+        {
+            id: 3,
+            trackName: 'tracks/russian-rock/Viktor_Coj_-_Zvezda_Po_Imeni_Solnce.mp3',
+            isHot: true,
+        }
+    ],
+    tracksRock: [
+        {
+            id: 0,
+            trackName: 'tracks/rock/Bob_Dylan_-_Knockin_On_Heaven_s_Door.mp3',
+            isHot: true,
+        },
+        {
+            id: 1,
+            trackName: 'tracks/rock/Queen_-_Bohemian_Rhapsody.mp3',
+            isHot: true,
+        },
+        {
+            id: 2,
+            trackName: 'tracks/rock/Queen_-_The_Show_Must_Go_On.mp3',
+            isHot: true,
+        },
+        {
+            id: 3,
+            trackName: 'tracks/rock/Queen_-_Who_Wants_To_Live_Forever.mp3',
+            isHot: true,
+        }
+    ],
+}
+
+// render
